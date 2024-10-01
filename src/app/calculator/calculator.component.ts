87a5a29dbff6156a3129ea7f66a139bda8fd5131
Balla Kavya <ballakavya594@gmail.com>
2024-10-01 10:00:00 +0530

    update

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './calculator.component.css'
 })
 export class CalculatorComponent {
-
+    result:number=0
+    addition(a:string,b:string):void{
+      const num1=parseInt(a);
+      const num2=parseInt(b)
+      this.result=num1+num2;
+    }
+     substraction(a:string,b:string):void{
+      const num1=parseInt(a);
+      const num2=parseInt(b)
+      this.result=num1-num2;
+    }   
+     multiplication(a:string,b:string):void{
+      const num1=parseInt(a);
+      const num2=parseInt(b)
+      this.result=num1*num2;
+    } 
+    Division(a:string,b:string):void{
+      const num1=parseInt(a);
+      const num2=parseInt(b)
+      this.result=num1/num2;
+    }
+    
 }
