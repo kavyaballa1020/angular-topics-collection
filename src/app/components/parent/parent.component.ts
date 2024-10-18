e2db396807c6addd7b5d10d25f429ac429564989
Balla Kavya <ballakavya594@gmail.com>
2024-10-18 10:00:00 +0530

    component communication

---
+++ src/app/components/parent/parent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  standalone: true,
+  imports: [],
+  templateUrl: './parent.component.html',
+  styleUrl: './parent.component.css'
+})
+export class ParentComponent {
+
+}
