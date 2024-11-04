91012a201281ba18608174e428b2c342f681c74d
Balla Kavya <ballakavya594@gmail.com>
2024-11-04 20:19:58 +0530

    "Added CalculatorComponent and OneWayBindingComponent with corresponding HTML and CSS files, and updated AppComponent to import these new components."

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { EmployeeComponent } from "./components/employee/employee.component";
 import { KavyaComponent } from "./components/kavya/kavya.component";
+import { BindingComponent } from "./components/binding/binding.component";
+import { CalculatorComponent } from "./components/calculator/calculator.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, RouterOutlet, HttpClientModule, EmployeeComponent, KavyaComponent], 
+  imports: [RouterModule, RouterOutlet, HttpClientModule, EmployeeComponent, KavyaComponent, BindingComponent, CalculatorComponent], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,33 +1,33 @@
-import { Component } from '@angular/core';
+  import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-calculator',
-  standalone: true,
-  imports: [],
-  templateUrl: './calculator.component.html',
-  styleUrl: './calculator.component.css'
-})
-export class CalculatorComponent {
-    result:number=0
-    addition(a:string,b:string):void{
-      const num1=parseInt(a);
-      const num2=parseInt(b)
-      this.result=num1+num2;
-    }
-     substraction(a:string,b:string):void{
-      const num1=parseInt(a);
-      const num2=parseInt(b)
-      this.result=num1-num2;
-    }   
-     multiplication(a:string,b:string):void{
-      const num1=parseInt(a);
-      const num2=parseInt(b)
-      this.result=num1*num2;
-    } 
-    Division(a:string,b:string):void{
-      const num1=parseInt(a);
-      const num2=parseInt(b)
-      this.result=num1/num2;
-    }
-    
-}
+  @Component({
+    selector: 'app-calculator',
+    standalone: true,
+    imports: [],
+    templateUrl: './calculator.component.html',
+    styleUrl: './calculator.component.css'
+  })
+  export class CalculatorComponent {
+      result:number=0
+      addition(a:string,b:string):void{
+        const num1=parseInt(a);
+        const num2=parseInt(b)
+        this.result=num1+num2;
+      }
+      substraction(a:string,b:string):void{
+        const num1=parseInt(a);
+        const num2=parseInt(b)
+        this.result=num1-num2;
+      }   
+      multiplication(a:string,b:string):void{
+        const num1=parseInt(a);
+        const num2=parseInt(b)
+        this.result=num1*num2;
+      } 
+      Division(a:string,b:string):void{
+        const num1=parseInt(a);
+        const num2=parseInt(b)
+        this.result=num1/num2;
+      }
+      
+  }
